69a980e766821296fd029c0609bda55d9ca183fa
宮本航平 <38koo@miyamotheinoAir.AirPort>
2024-01-10 01:33:56 +0900

    ??ルーセルの最初のアイテムと最後のアイテムの時の処理追加

--- src/components/Carousel/index.tsx
+++ src/components/Carousel/index.tsx
@@ -1,7 +1,7 @@
 import { stylex } from "@stylexjs/stylex";
 import { CarouselItem, CarouselItemProps } from "./CarouselItem";
 import { carouselStyle } from "./index.stylex";
-import { useRef } from "react";
+import { useRef, useState } from "react";
 
 type CarouselProps = {
   images: CarouselItemProps[];
@@ -9,18 +9,46 @@ type CarouselProps = {
 
 export const Carousel = ({ images }: CarouselProps) => {
   const ref = useRef<HTMLDivElement>(null);
+  const [isStartPosition, setIsStartPosition] = useState(true);
+  const [isLastPosition, setIsLastPosition] = useState(false);
 
   const scrollSlide = (direction: 300 | -300) => {
     ref.current?.scrollBy({ left: direction, behavior: "smooth" });
+
+    if (!ref.current) return;
+
+    // FIXME: ここのロジックもっときれいにしたい
+    if (direction === -300 && ref.current?.scrollLeft === 300) {
+      setIsStartPosition(true);
+      return;
+    }
+
+    if (
+      direction === 300 &&
+      ref.current?.scrollWidth - ref.current?.scrollLeft ===
+        ref.current.clientWidth + 300
+    ) {
+      setIsLastPosition(true);
+      return;
+    }
+
+    setIsStartPosition(false);
+    setIsLastPosition(false);
   };
 
   if (images.length === 0) return null;
 
   return (
     <div {...stylex.props(carouselStyle.base)}>
+      {/* TODO: アクセシビリティの観点から、buttonタグは連続した方が良さそう - フォーカスを連続させるため。 */}
       <button
-        onClick={() => scrollSlide(300)}
-        {...stylex.props(carouselStyle.button)}
+        onClick={isStartPosition ? () => {} : () => scrollSlide(-300)}
+        {...stylex.props(
+          carouselStyle.button,
+          isStartPosition
+            ? carouselStyle.inactiveButtonBackground
+            : carouselStyle.activeButtonBackground
+        )}
       >
         ←
       </button>
@@ -32,8 +60,13 @@ export const Carousel = ({ images }: CarouselProps) => {
         })}
       </div>
       <button
-        onClick={() => scrollSlide(-300)}
-        {...stylex.props(carouselStyle.button)}
+        onClick={isLastPosition ? () => {} : () => scrollSlide(300)}
+        {...stylex.props(
+          carouselStyle.button,
+          isLastPosition
+            ? carouselStyle.inactiveButtonBackground
+            : carouselStyle.activeButtonBackground
+        )}
       >
         →
       </button>
